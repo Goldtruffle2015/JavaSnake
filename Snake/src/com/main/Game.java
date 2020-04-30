14b68780536dd3dfd4f2857492241a9107606f9a
John Yu <goldtruffle2015@hotmail.com>
2020-04-30 16:55:37 -0600

    Changed the background to black. Added more code to a number of methods.

--- Snake/src/com/main/Game.java
+++ Snake/src/com/main/Game.java
@@ -5,41 +5,120 @@
 package com.main;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable {
-	
-	private static final long serialVersionUID = -668240625892092763L;
+	// -- Attributes -- //
+	private static final long serialVersionUID = -668240625892092763L;	// No idea what this is for
 
-	public static final int WIDTH = 1000;
-	public static final int HEIGHT = WIDTH *9/16;
+	// Screen Dimensions //
+	public static final int WIDTH = 1152;	// Sets window width to screen width
+	public static final int HEIGHT = 648;	// Sets window height to screen height
 	
-	public boolean running = false;
-	private Thread gameThread;
+	// Threading //
+	public boolean running = false;	// Define whether the game is running or not
+	private Thread gameThread;	// Creates a thread variable for game thread
 	
-	private Player player;
-	private Target target;
+	// Instances //
+	private Player player;	// Creates a variable for the player sprite
+	private Target target;	// Creates a variable for the target sprite
 	
+	// -- Constructor Method -- //
 	public Game() {
-		// -- Setup canvas -- //
-		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		this.setMaximumSize(new Dimension(WIDTH, HEIGHT));
-		this.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+		setUpCanvas();	// Set up the canvas
+		initialize();	// Initialize the player and target classes
 		
 		// -- Create the window -- //
 		new Window("Snake by John Yu", this);
 	}
-
+	
+	// -- Functions -- //
+	
+	private void initialize() {
+		// Initialize player
+		
+		// Initialize target
+	}
+	
+	public void setUpCanvas() {	// Sets the preferred dimensions of the canvas
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		this.setMaximumSize(new Dimension(WIDTH, HEIGHT));
+		this.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+	}
+	
+	// -- Methods -- //
 	@Override
-	public void run() {
+	public void run() {	// Runs the game
+		this.requestFocus();
+		// game timer
+		
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		// Looping starts here //
+		while (running) {	
+			long now = System.nanoTime();
+			delta +=(now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				update();	// Updates the sprites
+				delta--;
+			}
+			if (running) draw();	// Draws the updated sprites
+			frames++;
+			
+			if (System.currentTimeMillis() - timer < 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+
+	private void stop() {
+		
+	}
+
+	private void draw() {
+		// Initialize drawing tools //
+		BufferStrategy buffer = this.getBufferStrategy();
+		
+		if (buffer == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		// Draw background //
+		Graphics g = buffer.getDrawGraphics();
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		// Draw sprites //
+		
+		// Dispose, ACTUALLY draw //
+		g.dispose();
+		buffer.show();
+	}
+
+	private void update() {
 		
 	}
 
 	public void start() {
-		
+		gameThread = new Thread(this);
+		gameThread.start();
+		running = true;
 	}
-	
-	public static void main(String[] args) {
+	// -- Main Method -- //
+	public static void main(String[] args) {	 
 		new Game();
 	}
 }
--- Snake/src/com/main/Window.java
+++ Snake/src/com/main/Window.java
@@ -1,6 +1,6 @@
-// Title: Snake
-// Author: John Yu
-// Date: 2020-04-30
+//	The window class handles the main frame of the game
+//	This is where the entire game happens
+//
 
 package com.main;
 
@@ -8,13 +8,13 @@ import javax.swing.JFrame;
 
 public class Window {
 	public Window(String title, Game game) {
-		JFrame frame = new JFrame(title);
+		JFrame frame = new JFrame(title);	// Creates the raw window
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // X button exits window
 		frame.setResizable(false); // User cannot resize window
-		frame.add(game); 
-		frame.pack();
-		frame.setLocationRelativeTo(null);
+		frame.add(game);	// Game class is a component of the frame
+		frame.pack();	// Components of frame will fit onto window
+		frame.setLocationRelativeTo(null);	// Game will be centered in window
 		frame.setVisible(true);
 		
 		game.start();
